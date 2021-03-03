c4970dcaf9856a9f8d7660e9c6b3988d80d2f0db
Codenax <jahangir.vsv@gmail.com>
2021-03-03 16:38:02 +0800

    dextop view done

--- src/js/custom.js
+++ src/js/custom.js
@@ -29,3 +29,14 @@
 // $(function() {
 //     alert(usesViewport()?'Uses viewport':'Doesn\'t use viewport');
 // });
+
+
+$(".default_option").click(function(){
+    $(this).parent().toggleClass("active");
+  })
+  
+  $(".select_ul li").click(function(){
+    var currentele = $(this).html();
+    $(".default_option li").html(currentele);
+    $(this).parents(".select_wrap").removeClass("active");
+  })
